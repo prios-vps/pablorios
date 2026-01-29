b5ee281d5374af44cd84f17a525099104c96bbad
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-01-29 05:29:36 -0300

    Regenerando fuentes WOFF2 pasando pot TTF

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { ncm10Serif } from "@/styles/ts/fonts";
 import "../styles/css/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export const metadata: Metadata = {
   title: "Analista Programador - Pablo Ríos",
@@ -30,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${ncm10Serif.className} antialiased`}
       >
         {children}
       </body>
--- src/styles/ts/fonts.ts
+++ src/styles/ts/fonts.ts
@@ -1,30 +1,8 @@
 import localFont from 'next/font/local'
 
-// 1. FUENTE PRINCIPAL (SERIF) - Usando las variantes de tamaño 10
-export const ncmSerif = localFont({
-  src: [
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCM10-Book.woff2', weight: '400', style: 'normal' },
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCM10-BookItalic.woff2', weight: '400', style: 'italic' },
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCM10-Bold.woff2', weight: '700', style: 'normal' },
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCM10-BoldItalic.woff2', weight: '700', style: 'italic' },
-  ],
-  variable: '--font-ncm-serif'
-})
-
-// 2. FUENTE SANS (Sín remates)
-export const ncmSans = localFont({
-  src: [
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCMSans10-Book.woff2', weight: '400', style: 'normal' },
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCMSans10-Bold.woff2', weight: '700', style: 'normal' },
-  ],
-  variable: '--font-ncm-sans'
-})
-
-// 3. FUENTE MONO (Para código)
-export const ncmMono = localFont({
-  src: [
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCMMono10-Book.woff2', weight: '400', style: 'normal' },
-    { path: '../../public/fonts/NewComputerModern/woff2/NewCMMono10-Bold.woff2', weight: '700', style: 'normal' },
-  ],
-  variable: '--font-ncm-mono'
+// 1. FUENTE PRINCIPAL (SERIF)
+// 1.1 10 => Encabezados y fuentes mayores
+export const ncm10Serif = localFont({
+  src: '../../assets/fonts/NewComputerModern/woff2/NewCM10-Book.woff2',
+  variable: '--font-ncm-10-serif'
 })
