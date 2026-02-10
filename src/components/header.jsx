bef502a9f997642a314565869e017c58584e6a08
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-02-10 23:31:48 +0000

    Pruebas de ingreso y orden de proyectos

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
   // const isHome = pathname === '/';
 
   return (
-    <header className="flex justify-between items-center mb-12 gap-5">
+    <header className="flex flex-col min-sm:flex-row justify-between items-center mb-12 gap-5">
       <div id="hero">
         <h1 className="text-4xl ncm10serif font-bold not-italic">Pablo Ariel Ríos Pérez</h1>
         <h2 className="text-xl text-gray-800 dark:text-gray-400 mt-2 ncm10serif font-medium italic">
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,19 +1,48 @@
+export interface ProjectDetails {
+  title: string
+  skills: string[]
+  problem: string
+  solution: string
+  impact?: string
+}
+
 export interface Project {
   id: number
   title: string
   subtitle?: string
   description: string
   url?: {
-    details?: string
     demo?: string
+    download?: string
     src?: string
   }
+  details?: ProjectDetails[]
 }
 
 export const projects: Project[] = [
   {
     id: 1,
     title: 'Sistema de Toma de Pedidos para Restaurante',
+    subtitle: 'Integración con base de datos legacy y flujo completo de comandas',
+    description: 'Sistema de toma de pedidos para garzones con gestión de salones, mesas y comandas, búsqueda de productos, cálculo automático de totales y envío de pedidos a cocina mediante impresión automática de tickets, integrado a base de datos legacy existente.',
+    url: {
+      details: 'restaurant',
+      demo: 'https://restaurant.pablorios.cl'
+    }
+  },
+  {
+    id: 2,
+    title: 'Programa para toma de inventario de hardware',
+    subtitle: 'Escaner de hardware, imoresoras conectadas y registro de datos en Excel',
+    description: 'Aplicación gráfica probada en entornos Windows, escrita en Python + Tkinter, con el fin de capturar información acerca del hardware del equipo. Usa Pandas para procesar los datos y convertirlos a xlsx gracias a openpyxl. También es capaz de escanear las impresoras conectadas al equipo por red.',
+    url: {
+      details: 'escaner',
+      download: 'https://descargas.pablorios.cl/Escaner.zip'
+    }
+  },
+  {
+    id: 3,
+    title: 'Automatizador de informes en Excel',
     subtitle: 'Integración con base de datos legacy y flujo completo de comandas',
     description: 'Sistema de toma de pedidos para garzones con gestión de salones, mesas y comandas, búsqueda de productos, cálculo automático de totales y envío de pedidos a cocina mediante impresión automática de tickets, integrado a base de datos legacy existente.',
     url: {
