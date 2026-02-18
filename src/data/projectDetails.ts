fe3d9119a181dc2d6c7a8fde95fc453347e33d07
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-02-18 18:53:56 +0000

    Información de proyectos actualizada x tipos

---
+++ src/data/projectDetails.ts
@@ -0,0 +1,126 @@
+import ProjectDetail from '@/types/projectDetail';
+
+export const projectDetails: ProjectDetail[] =
+    [
+        {
+            id: 1,
+            idProject: 1,
+            title: "Primer problema 1",
+            skills: [
+                "Competencia 1",
+                "Competencia 2",
+                "Competencia 3"
+            ],
+            problem: "Problema 1",
+            solution: "Solución 1",
+            impact: "Impacto 1"
+        },
+        {
+            id: 2,
+            idProject: 1,
+            title: "Segundo problema 1",
+            skills: [
+                "Competencia 1",
+                "Competencia 3",
+                "Competencia 5"
+            ],
+            problem: "Problema 2",
+            solution: "Solución 2",
+            impact: "Impacto 2"
+        },
+        {
+            id: 3,
+            idProject: 1,
+            title: "Tercer problema 1",
+            skills: [
+                "Competencia 3"
+            ],
+            problem: "Problema 3",
+            solution: "Solución 3"
+        },
+        {
+            id: 4,
+            idProject: 1,
+            title: "Cuarto problema 1",
+            skills: [
+                "Competencia 2",
+                "Competencia 4",
+                "Competencia 6"
+            ],
+            problem: "Problema 4",
+            solution: "Solución 4",
+            impact: "Impacto 4"
+        },
+        {
+            id: 5,
+            idProject: 1,
+            title: "Quinto problema 1",
+            skills: [
+                "Competencia 5",
+                "Competencia 6"
+            ],
+            problem: "Problema 5",
+            solution: "Solución 5"
+        },
+        {
+            id: 6,
+            idProject: 2,
+            title: "Primer problema 2",
+            skills: [
+                "Competencia 1",
+                "Competencia 2",
+                "Competencia 3"
+            ],
+            problem: "Problema 1",
+            solution: "Solución 1",
+            impact: "Impacto 1"
+        },
+        {
+            id: 7,
+            idProject: 2,
+            title: "Segundo problema 2",
+            skills: [
+                "Competencia 1",
+                "Competencia 3",
+                "Competencia 5"
+            ],
+            problem: "Problema 2",
+            solution: "Solución 2",
+            impact: "Impacto 2"
+        },
+        {
+            id: 8,
+            idProject: 2,
+            title: "Tercer problema 2",
+            skills: [
+                "Competencia 3"
+            ],
+            problem: "Problema 3",
+            solution: "Solución 3"
+        },
+        {
+            id: 9,
+            idProject: 2,
+            title: "Cuarto problema 2",
+            skills: [
+                "Competencia 2",
+                "Competencia 4",
+                "Competencia 6"
+            ],
+            problem: "Problema 4",
+            solution: "Solución 4",
+            impact: "Impacto 4"
+        },
+        {
+            id: 10,
+            idProject: 2,
+            title: "Quinto problema 2",
+            skills: [
+                "Competencia 5",
+                "Competencia 6"
+            ],
+            problem: "Problema 5",
+            solution: "Solución 5"
+        },
+];
+
