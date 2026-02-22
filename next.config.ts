1f881cdcde8fa41e78e39549fccaafeb34b99b40
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-02-22 08:57:19 +0000

    Corregido dual-theme en rehype-pretty-code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx';
+//import rehypePrettyCode, { type Options } from "rehype-pretty-code";
+
+/** @type {import('rehype-pretty-code').Options} */
+const rehypeOptions = {
+  theme: {
+    dark: "houston",
+    light: "catppuccin-latte",
+  },
+  keepBackground: true,
+};
 
 const nextConfig: NextConfig = {
+	reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   reactCompiler: true,
   output: 'export',
@@ -14,19 +25,7 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   options: {
     remarkPlugins: ['remark-gfm'],
-    rehypePlugins: [
-      [
-        'rehype-pretty-code',
-        {
-          theme: {
-            light: 'github-light',
-            dark: 'github-dark'
-          },
-          defaultColor: 'light',
-          cssVariablePrefix: '--shiki-',
-        },
-      ],
-    ],
+    rehypePlugins: [['rehype-pretty-code', rehypeOptions]],
   },
 });
 
