b789063e6c58423da68acb5beb683d5977fc1cd2
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-03-05 20:11:28 -0300

    Editando pagina general competencias

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@
 import type { Metadata } from "next";
 
 // Fuentes
-import {
+/*import {
   ncm10Serif,
   ncm08Serif,
   ncm10Sans,
   ncm08Sans,
   ncm10Mono,
 } from "@/styles/ts/fonts";
-
+ */
 // Componentes
 import Header from "../components/header";
 import Footer from "../components/footer";
@@ -36,8 +36,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body
+      {/*<body
         className={`${ncm10Serif.variable} ${ncm08Serif.variable} ${ncm10Sans.variable} ${ncm08Sans.variable} ${ncm10Mono.variable} antialiased bg-gray-300 text-gray-900 dark:bg-gray-800 dark:text-gray-200`}
+      >*/}
+      <body
+        className={`antialiased bg-gray-300 text-gray-900 dark:bg-gray-800 dark:text-gray-200`}
       >
         <main className="flex flex-col max-w-7xl mx-auto p-8">
           <Header />
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,17 +1,22 @@
+// Componentes de node
 import Link from 'next/link';
 
+// Componentes internos
+import T1 from '@/components/t1';
+
+// Información
 import { skills } from '@/data/ts/skills';
 
 export default function SkillIndex() {
   return (
     <>
-      <h1 className="text-3xl font-bold mb-6">Competencias</h1>
-      <ul className="flex flex-col justify-between gap-2 flex-wrap md:flex-row">
+      <T1>Competencias</T1>
+      <ul className="flex flex-col justify-center gap-3 flex-wrap md:flex-row">
         {skills.map((skill) => (
-          <li key={skill.id} className="flex justify-center min-w-60 border rounded-lg p-2 shadow-sm hover:shadow-md transition">
+          <li key={skill.id} className="flex m-0 p-0">
             <Link
               href={`/skills/${skill.slug}`}
-              className="flex"
+              className="flex justify-center min-w-60 border rounded-lg p-2 text-xl font-bold shadow-sm hover:shadow-md dark:hover:bg-gray-200 dark:hover:text-gray-800 transition"
             >
               {skill.name}
             </Link>
