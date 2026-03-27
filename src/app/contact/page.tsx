b68638714880b6e4f745de008c7ab23ec160c93d
Pablo Ríos <pablo_rios_p@protonmail.com>
2026-03-27 21:04:57 +0000

    Número de columnas variables en input

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,12 +5,12 @@ export default function ContactPage() {
   return (
     <>
       <T2>Formulario de Contacto</T2>
-      <form action='' method='POST' className='flex flex-col gap-2'>
+      <form action='' method='POST' className='flex flex-col gap-2 w-full max-w-lg'>
         <FormInput name='Nombre' />
         <FormInput name='Empresa' requiredField={false} />
         <FormInput name='Correo Electrónico' />
         <FormInput name='Teléfono' requiredField={false} />
-        <FormInput name='Mensaje' inputType="textarea" />
+        <FormInput name='Mensaje' maxCols={1} inputType="textarea" />
         <input type='submit' value='Enviar' className='' />
       </form>
     </>
--- src/components/struct/form-input.tsx
+++ src/components/struct/form-input.tsx
@@ -4,18 +4,23 @@ import { toSlug } from '@/lib/utils';
 
 interface Props {
   name: string;
+  maxCols?: number;
   inputType?: string;
   placeholder?: string;
   requiredField?: boolean;
 }
 
-export default function FormInput({name, inputType='text', placeholder='', requiredField=true}: Props) {
+export default function FormInput({name, maxCols=2, inputType='text', placeholder='', requiredField=true}: Props) {
   const slug = toSlug(name);
   const inputCommonClasses = 'flex dark:bg-neutral-900 border dark:border-stone-300 rounded-xs p-1';
   const optionalText = requiredField ? '' : ' (Opcional)';
+  let widthClasses = '';
+
+  if (maxCols > 1) widthClasses += ' md:w-1/2';
+  if (maxCols > 2) widthClasses += ' lg:w-1/3';
 
   return (
-    <div className='flex flex-col gap-1'>
+    <div className={`flex flex-col gap-1 w-full${widthClasses}`}>
       <label htmlFor={slug} className='text-sm font-bold'>{`${name}${optionalText}`}</label>
       {
         inputType === 'textarea'
